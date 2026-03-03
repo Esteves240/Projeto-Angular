9977391bd4b0ba71a2497055d2b3fa3b556a3fef
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-03 20:03:28 +0000

    feat: set up routing and integrate Menu component in Home page

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // default path shows the home component
+  { path: '', component: Home },
+  // catch-all redirect back to home
+  { path: '**', redirectTo: '' },
+];
