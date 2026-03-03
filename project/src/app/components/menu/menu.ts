502773502c7fc3b005cada53c82a37c238ee2e75
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-03 22:22:12 +0000

    feat: implement Menu component with logo and navigation links

--- project/src/app/components/menu/menu.ts
+++ project/src/app/components/menu/menu.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
