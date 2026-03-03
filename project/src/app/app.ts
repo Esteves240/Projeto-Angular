45e34e1e09657f0a1db835357cb9fbce640cf5b3
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-03 22:22:29 +0000

    fix: update imports in App component to use MatIconModule instead of Menu

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Menu } from './components/menu/menu';
+import { MatIconModule } from '@angular/material/icon'; //o minino não gosta deste
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
