2eae702d082638874678623a0509cff80a31d575
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-04 00:11:42 +0000

    feat: enhance Home page with album catalog and image

--- project/src/app/app.ts
+++ project/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+//Angular Material
 import { MatIconModule } from '@angular/material/icon'; //o minino não gosta deste
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatIconModule],
+  imports: [RouterOutlet, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
