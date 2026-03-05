0fd3e1750dc0899511c89c4f88eae0e32c198269
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-05 10:31:43 +0000

    fix: solve Angular Material table imports

--- project/src/app/pages/crud/crud.ts
+++ project/src/app/pages/crud/crud.ts
@@ -3,16 +3,31 @@ import { Menu } from '../../components/menu/menu';
 import { Button } from '../../components/button/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-crud',
   standalone: true,
-  imports: [Menu, Button, MatFormFieldModule, MatInputModule],
+  imports: [
+    Menu,
+    Button,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatTableModule,
+    MatSortModule,
+  ],
   templateUrl: './crud.html',
   styleUrl: './crud.css',
 })
 export class Crud {
-  dataSource: any;
+  displayedColumns: string[] = ['id', 'title', 'band', 'genre', 'action'];
+
+  dataSource = new MatTableDataSource<any>([]);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
