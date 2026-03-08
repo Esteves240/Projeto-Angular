d3def4e98c0d710169edef8be8bd1efa0acea7dc
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-08 02:44:59 +0000

    fix: empty route

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -3,10 +3,7 @@ import { Home } from './pages/home/home';
 import { Crud } from './pages/crud/crud';
 
 export const routes: Routes = [
-  // // default path shows the home component
-  // { path: '', component: Home },
-  // // catch-all redirect back to home
-  // { path: '**', redirectTo: '' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // rota inicial
   { path: 'home', component: Home },
   { path: 'crud', component: Crud },
 ];
