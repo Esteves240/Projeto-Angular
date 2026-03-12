bf0f13b409aa499a50d533e5d6794227bb10fe85
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-12 11:10:23 +0000

    ng lint && ng build

--- project/eslint.config.js
+++ project/eslint.config.js
@@ -15,27 +15,7 @@ module.exports = defineConfig([
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+      '@typescript-eslint/no-unused-vars': 'error',
     },
-  },
-  {
-    files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
   },
 ]);
