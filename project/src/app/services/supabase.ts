de4c226d03c2277ffcbb6ef21d1713072ffd68c5
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-03-17 14:22:03 +0000

    Add: binding logic to Supabase and CRrud methods

--- project/src/app/services/supabase.ts
+++ project/src/app/services/supabase.ts
@@ -1,6 +1,46 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root',
+providedIn: 'root'
 })
-export class Supabase {}
+
+
+export class SupabaseService {
+private supabase: SupabaseClient;
+constructor() {
+this.supabase = createClient(
+environment.supabaseUrl,
+environment.supabaseKey
+);
+}
+
+async getUsers(): Promise<User[]> {
+const { data, error } = await this.supabase
+.from('users')
+.select('*')
+.order('created_at', { ascending: false });
+if (error) {
+console.error('Erro ao listar utilizadores:', error.message);
+return [];
+}
+return data as User[];
+}
+
+async adUsers(title: string): Promise<User | null> {
+const { data, error } = await this.supabase
+.from('users')
+.insert([{ title, completed: false }])
+.select()
+.single();
+if (error) {
+console.error('Erro ao criar utilizador:', error.message);
+return null;
+}
+return data as User;
+}
+
+
+}
